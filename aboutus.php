0998fdb7b7984afb3060e9ce5c4c68a1eee3222e
ilopez19 <51893078+ilopez19@users.noreply.github.com>
2019-11-20 22:18:15 -0500

    PHP Website
    In the proccess of making a website

---
+++ aboutus.php
@@ -0,0 +1,17 @@
+<?php
+#parset("PHP File Header.php")
+
+include 'www/header.php';
+?>
+
+<?php
+// add code here
+
+
+?>
+
+<?php
+
+require_once('www/footer.php');
+?>
+
---
+++ details.php
@@ -0,0 +1,16 @@
+<?php
+#parset("PHP File Header.php")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+// add code here
+?>
+</body>
+</html>
